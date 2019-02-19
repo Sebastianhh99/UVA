6298264d83e59a086a6b7fbe210d564fea7a88f8
Sebastianhh99 <39110763+Sebastianhh99@users.noreply.github.com>
2019-02-19 18:59:15 -0500

    Update 195.cpp

--- 195.cpp
+++ 195.cpp
@@ -1,23 +1,27 @@
 #include <bits/stdc++.h>
 
-//Falta ordenar en orden alfabetico
-
 using namespace std;
 
 string word;
 vector<string> ans;
 
+bool comparator(char a,char b){
+    if(tolower(a)==tolower(b)){
+        return a<b;
+    }
+    return tolower(a)<tolower(b);
+}
+
 int main(){
-    ofstream archivo;
-    archivo.open("test.txt",ios::out);
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
+        ans.clear();
         cin>>word;
-        sort(word.begin(),word.end());
-        archivo<<word<<endl;
-        while(next_permutation(word.begin(),word.end())){
-            archivo<<word<<endl;
+        sort(word.begin(),word.end(),comparator);
+        cout<<word<<endl;
+        while(next_permutation(word.begin(),word.end(),comparator)){
+            cout<<word<<endl;
         }
     }
 }
